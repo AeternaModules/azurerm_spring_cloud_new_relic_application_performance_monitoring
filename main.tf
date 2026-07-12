d2a9fcbe5fa003a63660cc185b09dfe28f1641bb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:26 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+data "azurerm_key_vault_secret" "license_key" {
+  for_each     = { for k, v in var.spring_cloud_new_relic_application_performance_monitorings : k => v if v.license_key_key_vault_id != null && v.license_key_key_vault_secret_name != null }
+  name         = each.value.license_key_key_vault_secret_name
+  key_vault_id = each.value.license_key_key_vault_id
+}
 resource "azurerm_spring_cloud_new_relic_application_performance_monitoring" "spring_cloud_new_relic_application_performance_monitorings" {
   for_each = var.spring_cloud_new_relic_application_performance_monitorings
 
   app_name                        = each.value.app_name
-  license_key                     = each.value.license_key
+  license_key                     = each.value.license_key != null ? each.value.license_key : try(data.azurerm_key_vault_secret.license_key[each.key].value, null)
   name                            = each.value.name
   spring_cloud_service_id         = each.value.spring_cloud_service_id
   agent_enabled                   = each.value.agent_enabled
--- variables.tf
+++ variables.tf
@@ -4,6 +4,8 @@ Map of spring_cloud_new_relic_application_performance_monitorings, attributes be
 Required:
     - app_name
     - license_key
+    - license_key_key_vault_id (alternative to license_key - read from Key Vault instead)
+    - license_key_key_vault_secret_name (alternative to license_key - read from Key Vault instead)
     - name
     - spring_cloud_service_id
 Optional:
@@ -18,18 +20,20 @@ Optional:
 EOT
 
   type = map(object({
-    app_name                        = string
-    license_key                     = string
-    name                            = string
-    spring_cloud_service_id         = string
-    agent_enabled                   = optional(bool) # Default: true
-    app_server_port                 = optional(number)
-    audit_mode_enabled              = optional(bool)
-    auto_app_naming_enabled         = optional(bool)
-    auto_transaction_naming_enabled = optional(bool) # Default: true
-    custom_tracing_enabled          = optional(bool) # Default: true
-    globally_enabled                = optional(bool)
-    labels                          = optional(map(string))
+    app_name                          = string
+    license_key                       = string
+    license_key_key_vault_id          = optional(string)
+    license_key_key_vault_secret_name = optional(string)
+    name                              = string
+    spring_cloud_service_id           = string
+    agent_enabled                     = optional(bool) # Default: true
+    app_server_port                   = optional(number)
+    audit_mode_enabled                = optional(bool)
+    auto_app_naming_enabled           = optional(bool)
+    auto_transaction_naming_enabled   = optional(bool) # Default: true
+    custom_tracing_enabled            = optional(bool) # Default: true
+    globally_enabled                  = optional(bool)
+    labels                            = optional(map(string))
   }))
 }
 
