68bb0148f1806cdd13fd2afb09f78c96ce9fe2b9
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:38:25 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ Map of spring_cloud_new_relic_application_performance_monitorings, attributes be
 Required:
     - app_name
     - license_key
-    - license_key_key_vault_id (alternative to license_key - read from Key Vault instead)
-    - license_key_key_vault_secret_name (alternative to license_key - read from Key Vault instead)
+    - license_key_key_vault_id (optional, alternative to license_key)
+    - license_key_key_vault_secret_name (optional, alternative to license_key)
     - name
     - spring_cloud_service_id
 Optional:
@@ -26,12 +26,12 @@ EOT
     license_key_key_vault_secret_name = optional(string)
     name                              = string
     spring_cloud_service_id           = string
-    agent_enabled                     = optional(bool) # Default: true
+    agent_enabled                     = optional(bool)
     app_server_port                   = optional(number)
     audit_mode_enabled                = optional(bool)
     auto_app_naming_enabled           = optional(bool)
-    auto_transaction_naming_enabled   = optional(bool) # Default: true
-    custom_tracing_enabled            = optional(bool) # Default: true
+    auto_transaction_naming_enabled   = optional(bool)
+    custom_tracing_enabled            = optional(bool)
     globally_enabled                  = optional(bool)
     labels                            = optional(map(string))
   }))
